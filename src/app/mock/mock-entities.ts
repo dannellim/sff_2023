bae0630bca48f29e322bd61d6231922fda50c49e
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-09-09 18:19:39 +0800

    mock data for entities

---
+++ src/app/mock/mock-entities.ts
@@ -0,0 +1,34 @@
+import { Entity } from "../models/entity";
+
+export const ENTITIES: Entity[] = [
+    {
+        id: 1, image: 'credit_agricole_group', name: 'Crédit Agricole Group',
+        summary: "Crédit Agricole Group, sometimes called La banque verte (English: The green bank) due to its historical ties to farming, is a French international banking group and the world's largest cooperative financial institution.",
+        description: ""
+    },
+    {
+        id: 2, image: 'amundi', name: 'Amundi',
+        summary: "Amundi is a French asset management company. With €2 trillion of assets under management (AUM) at the end of 2021, it is the largest asset manager in Europe and one of the 10 biggest investment managers in the world.",
+        description: ""
+    },
+    {
+        id: 3, image: 'azqore', name: 'Azqore SA',
+        summary: " Azqore was established as CA-PBS in 1992 in the heart of private banking in Switzerland and is today an expert digital transformation partner for tomorrow’s wealth managers.",
+        description: ""
+    },
+    {
+        id: 4, image: 'credit_agricole_cib', name: 'Crédit Agricole CIB',
+        summary: "Crédit Agricole CIB is the Corporate and Investment Banking arm of the Crédit Agricole Group, the 10th largest banking group worldwide in terms of balance sheet size.",
+        description: ""
+    },
+    {
+        id: 5, image: 'caceis', name: 'Caecis',
+        summary: " CACEIS is the asset servicing banking group of Crédit Agricole and Santander dedicated to asset managers, insurance companies, pension funds, banks, private equity and real estate funds, brokers and corporate clients.",
+        description: ""
+    },
+    {
+        id: 6, image: 'iwm', name: 'Indosuez Wealth Management',
+        summary: "Indosuez Wealth Management supports its clients residing in Singapore with the creation, protection, management and transmission of their wealth.",
+        description: ""
+    },
+];
