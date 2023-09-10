3c8cc48286318633bbcf00ca6a5edaa6ffefb3f1
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-09-10 08:57:50 +0800

    add main page group logo and summary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ export class AppComponent {
   title = 'Welcome to';
   subtitle = 'Crédit Agricole Group'
   info = 'Financial Services'
+  summary = "Crédit Agricole Group, sometimes called La banque verte (English: The green bank) due to its historical ties to farming, is a French international banking group and the world's largest cooperative financial institution."
 }
