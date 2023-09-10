db08d601d9bb9f2ffbbd4972f2afcd242abcd76e
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-09-10 19:07:29 +0800

    add main group entity

--- src/app/mock/mock-entities.ts
+++ src/app/mock/mock-entities.ts
@@ -1,11 +1,11 @@
 import { Entity } from "../models/entity";
 
 export const ENTITIES: Entity[] = [
-    // {
-    //     id: 1, image: 'credit_agricole_group', name: 'Crédit Agricole Group',
-    //     summary: "Crédit Agricole Group, sometimes called La banque verte (English: The green bank) due to its historical ties to farming, is a French international banking group and the world's largest cooperative financial institution.",
-    //     description: ""
-    // },
+    {
+        id: 1, image: 'credit_agricole_group', name: 'Crédit Agricole Group',
+        description: "Crédit Agricole Group, sometimes called La banque verte (English: The green bank) due to its historical ties to farming, is a French international banking group and the world's largest cooperative financial institution.",
+        summary: "International Banking Group"
+    },
     {
         id: 2, image: 'amundi', name: 'Amundi',
         description: "Amundi is a French asset management company. With €2 trillion of assets under management (AUM) at the end of 2021, it is the largest asset manager in Europe and one of the 10 biggest investment managers in the world.",
@@ -19,16 +19,16 @@ export const ENTITIES: Entity[] = [
     {
         id: 4, image: 'credit_agricole_cib', name: 'Crédit Agricole CIB',
         description: "Crédit Agricole CIB is the Corporate and Investment Banking arm of the Crédit Agricole Group, the 10th largest banking group worldwide in terms of balance sheet size.",
-        summary: "Capital markets and corporate investment banking"
+        summary: "Capital Markets and Corporate Investment Banking"
     },
     {
         id: 5, image: 'caceis', name: 'Caecis',
         description: " CACEIS is the asset servicing banking group of Crédit Agricole and Santander dedicated to asset managers, insurance companies, pension funds, banks, private equity and real estate funds, brokers and corporate clients.",
-        summary: "Global depository"
+        summary: "Global Depository"
     },
     {
         id: 6, image: 'iwm', name: 'Indosuez Wealth Management',
         description: "Indosuez Wealth Management supports its clients residing in Singapore with the creation, protection, management and transmission of their wealth. With 130 billion euros in assets under management (as of 31/12/2022), Indosuez Wealth Management is one of the world's leading wealth management companies.",
-        summary: "Private banking"
+        summary: "Private Banking"
     },
 ];
