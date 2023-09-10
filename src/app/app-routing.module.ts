cdd921310c5d6328f284bf5291b8d200cb56b2db
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-09-10 19:08:28 +0800

    add routing and default routing to entity list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntitiesComponent } from './entities/entities.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/entities', pathMatch: 'full' },
+  { path: 'entities', component: EntitiesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
