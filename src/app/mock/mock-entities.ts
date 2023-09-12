0537ecaa02de3398595ddef39354a4a3aedd72df
Dannel Lim Khye Hsen <dannel.limkhyehsen@azqore.com>
2023-09-12 12:40:18 +0800

    Update data for azqore, iwm and caesis

--- src/app/mock/mock-entities.ts
+++ src/app/mock/mock-entities.ts
@@ -2,33 +2,33 @@ import { Entity } from "../models/entity";
 
 export const ENTITIES: Entity[] = [
     {
-        id: 1, image: 'credit_agricole_group', name: 'Crédit Agricole Group',
+        id: 1, image: 'credit_agricole_group.svg', name: 'Crédit Agricole Group',
         description: "Crédit Agricole Group, sometimes called La banque verte (English: The green bank) due to its historical ties to farming, is a French international banking group and the world's largest cooperative financial institution.",
         summary: "International Banking Group"
     },
     {
-        id: 2, image: 'amundi', name: 'Amundi',
+        id: 2, image: 'amundi.svg', name: 'Amundi',
         description: "Amundi is a French asset management company. With €2 trillion of assets under management (AUM) at the end of 2021, it is the largest asset manager in Europe and one of the 10 biggest investment managers in the world.",
         summary: "Asset Management"
     },
     {
-        id: 3, image: 'azqore', name: 'Azqore SA',
-        description: " Azqore was established as CA-PBS in 1992 in the heart of private banking in Switzerland and is today an expert digital transformation partner for tomorrow’s wealth managers.",
-        summary: "IT & BPO"
+        id: 3, image: 'azqore.svg', name: 'Azqore SA',
+        description: "A Swiss headquartered company with offices in Singapore covering Asian clients, Azqore is a global company that is committed to providing private banks/wealth managers with integrated turnkey solutions for their core banking system, back office operations and digital needs. With a proven track record of over 30 years, Azqore is a trusted provider with SFA Fintech certification, OSPAR certified on ABS list, ISAE 3402 & 3000, ISO9001, ISO27001, Swift Bureau Service certified and GDPR compliant. In addition, Azqore provides consulting services to efficiently transition to our platform and optimize the use of our products and services. We operate an open platform that could be connected to other fintechs in our marketplace as well as our clients’ existing systems where compatible. Our key differentiator is our banking DNA, coupled with technology expertise, makes us an ideal partner for Private Banks/Wealth Managers.​ Being part of a larger financial services group means we are also able to offer solutions across business lines by partnering with one or more of our sister companies.​",
+        summary: "Wealth Management Outsourced Service Provider & Fintech SaaS, BPO, Digital Transformation, Consulting​"
     },
     {
-        id: 4, image: 'credit_agricole_cib', name: 'Crédit Agricole CIB',
+        id: 4, image: 'credit_agricole_cib.svg', name: 'Crédit Agricole CIB',
         description: "Crédit Agricole CIB is the Corporate and Investment Banking arm of the Crédit Agricole Group, the 10th largest banking group worldwide in terms of balance sheet size.",
         summary: "Capital Markets and Corporate Investment Banking"
     },
     {
-        id: 5, image: 'caceis', name: 'Caecis',
-        description: " CACEIS is the asset servicing banking group of Crédit Agricole and Santander dedicated to asset managers, insurance companies, pension funds, banks, private equity and real estate funds, brokers and corporate clients.",
-        summary: "Global Depository"
+        id: 5, image: 'caceis.svg', name: 'Caecis',
+        description: "CACEIS is the asset servicing banking group of Crédit Agricole and Santander dedicated to asset managers, insurance companies, pension funds, banks, private equity and real estate funds, brokers and corporate clients. Through offices across Europe, North and South America and Asia, CACEIS offers a broad range of services  covering execution, clearing, forex, securities lending, custody, depositary and fund administration, fund distribution support, middle office outsourcing and issuer services. With assets under custody of €4.6 trillion and assets under administration of €2.2 trillion, CACEIS is a European leader in asset servicing and one of the major players worldwide (figures as of 31 December 2022). ​",
+        summary: "Asset Servicing​"
     },
     {
-        id: 6, image: 'iwm', name: 'Indosuez Wealth Management',
-        description: "Indosuez Wealth Management supports its clients residing in Singapore with the creation, protection, management and transmission of their wealth. With 130 billion euros in assets under management (as of 31/12/2022), Indosuez Wealth Management is one of the world's leading wealth management companies.",
-        summary: "Private Banking"
+        id: 6, image: 'iwm_brown.png', name: 'Indosuez Wealth Management',
+        description: "Indosuez Wealth Management is the global wealth management brand of Crédit Agricole Group, ranked 10th largest bank in the world based on balance sheet (The Banker 2022). We bring families and entrepreneurs a human-scale organization backed by one of Europe’s largest and most robust universal banking groups. Shaped by more than 145 years of experience supporting families and entrepreneurs around the world, Indosuez Wealth Management offers a tailor-made approach that allows our clients to protect and develop their wealth in alignment with their aspirations. Its teams take a comprehensive view and provide expert advice and exceptional service, offering a wide range of services for the management of both personal and business assets.​ Renowned for its international reach combined with a human scale, Indosuez Wealth Management employs about 3,000 people in Europe, Asia-Pacific and the Middle East.​",
+        summary: "Access to key Asian markets • Asian mandates and local currencies • Private equity • Credit solutions • Wealth structuring • Discretionary portfolio management • Mutual funds • Structured products • Advisory mandates • Hedge funds advisory mandates • Forex and precious metals • Insurance premium financing (IPF) • Sustainable investing (ESG)​"
     },
 ];
