d304707d8b1335a82a6ae4a27030a819f6daf5a1
Dannel Lim Khye Hsen <dannel.limkhyehsen@azqore.com>
2023-09-18 18:11:40 +0800

    fix stupid errors (andrei)

--- src/app/components/entities/entities.component.ts
+++ src/app/components/entities/entities.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Entity } from '../../models/entity';
 import { EntityService } from '../../services/entity-service/entity.service';
+import { LoaderService } from 'src/app/services/loader/loader.service';
 
 @Component({
   selector: 'app-entities',
@@ -8,14 +9,19 @@ import { EntityService } from '../../services/entity-service/entity.service';
   styleUrls: ['./entities.component.css']
 })
 export class EntitiesComponent {
-  constructor(private entityService: EntityService) { }
+  constructor(private entityService: EntityService, private loader: LoaderService) { }
   entities: Entity[] = [];
-  groupEntity!: Entity;
+  groupEntity?: Entity;
   ngOnInit(): void {
     this.getEntities();
   }
   getEntities(): void {
+    this.loader.setLoading(true);
     this.entityService.getEntities()
-      .subscribe(entities => { this.entities = entities.splice(1); this.groupEntity = entities[0]; });
+      .subscribe(entities => {
+        this.entities = entities.splice(1);
+        this.groupEntity = entities[0];
+        this.loader.setLoading(false);
+      });
   }
 }
--- src/app/components/entity-detail/entity-detail.component.ts
+++ src/app/components/entity-detail/entity-detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Entity } from '../../models/entity';
 import { ActivatedRoute } from '@angular/router';
 import { EntityService } from '../../services/entity-service/entity.service';
+import { LoaderService } from 'src/app/services/loader/loader.service';
 
 @Component({
   selector: 'app-entity-detail',
@@ -10,15 +11,16 @@ import { EntityService } from '../../services/entity-service/entity.service';
 })
 
 export class EntityDetailComponent {
-  constructor(private route: ActivatedRoute, private entityService: EntityService) { }
+  constructor(private route: ActivatedRoute, private entityService: EntityService, private loader: LoaderService) { }
   entities: Entity[] = [];
   ngOnInit(): void {
     this.getEntity();
   }
   selectedEntity?: Entity;
   getEntity(): void {
+    this.loader.setLoading(true);
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.entityService.getEntities()
-      .subscribe(entities => { this.selectedEntity = entities.find(i => i.id === id); });
+      .subscribe(entities => { this.selectedEntity = entities.find(i => i.id === id); this.loader.setLoading(false); });
   }
 }
