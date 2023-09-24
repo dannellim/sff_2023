ae4949691987dbee48212ac7a4f976380ea16e18
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-09-24 22:35:41 +0800

    add simple authorisation to api

--- src/app/services/scan/scan.service.ts
+++ src/app/services/scan/scan.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, retry } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Constants } from '../../constants';
 
+const headers = new HttpHeaders().set('DANNEL_IS_THE_BEST', 'YES')
 @Injectable({
   providedIn: 'root'
 })
 export class ScanService {
   constructor(private http: HttpClient) { }
   postScanData(scan: string): Observable<string> {
-    return this.http.get<string>(Constants.POST_SCAN_DATA_API + "?text=" + encodeURIComponent(scan));
+    return this.http.get<string>(Constants.POST_SCAN_DATA_API + "?text=" + encodeURIComponent(scan), { 'headers': headers });
   }
 }
