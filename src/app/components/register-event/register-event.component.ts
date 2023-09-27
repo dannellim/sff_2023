49ffe1b7cd4cecc5c29668d7d7b6eef299da4f3d
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-09-27 23:26:30 +0800

    event registration

--- src/app/components/register-event/register-event.component.ts
+++ src/app/components/register-event/register-event.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Helper } from 'src/app/helper';
+import { EVENTS } from 'src/app/mock/mock-events';
+import { SPEAKERS } from 'src/app/mock/mock-speakers';
+import { Speaker } from 'src/app/models/speaker';
+import { LoaderService } from 'src/app/services/loader/loader.service';
+import { Event } from 'src/app/models/event';
+import { Register } from 'src/app/models/register';
 
 @Component({
   selector: 'app-register-event',
@@ -6,5 +14,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-event.component.css']
 })
 export class RegisterEventComponent {
-
+  events: Event[] = EVENTS;
+  speakers: Speaker[] = SPEAKERS;
+  monthAsString(arg0: number) {
+    return Helper.monthAsString(arg0);
+  }
+  dayOfWeekAsString(arg0: number) {
+    return Helper.dayOfWeekAsString(arg0);
+  }
+  constructor(private route: ActivatedRoute, private loader: LoaderService) { }
+  ngOnInit(): void {
+    this.loader.setLoading(true);
+    this.getEvent();
+    this.loader.setLoading(false);
+  }
+  selectedEvent?: Event;
+  eventDate?: Date;
+  speaker?: Speaker
+  getEvent(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.selectedEvent = this.events.find(i => i.id === id);
+    this.contact.eventId = id;
+    this.contact.eventTitle = this.selectedEvent?.name ?? '';
+    var [DD, MM, YYYY] = this.selectedEvent!.date.split('/');
+    this.eventDate = new Date(YYYY + "-" + MM + "-" + DD);
+    this.getSpeaker();
+  }
+  getSpeaker(): void {
+    const id = this.selectedEvent?.speakerId;
+    this.speaker = this.speakers.find(i => i.id === id);
+  }
+  contact: Register = {
+    email: '', name: '', eventTitle: '', eventId: 0
+  };
+  submit() {
+    console.log(this.contact);
+  }
 }
---
+++ src/app/models/register.ts
@@ -0,0 +1,6 @@
+export interface Register {
+    email: string;
+    name: string;
+    eventId: number;
+    eventTitle: string;
+}
