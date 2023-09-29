0421f35503d957d49ae301afca67bb3c3e867a7b
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-09-29 21:13:41 +0800

    add speaker service feature and api

--- src/app/services/speaker/speaker-service.service.ts
+++ src/app/services/speaker/speaker-service.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Constants } from 'src/app/constants';
+import { Speaker } from 'src/app/models/speaker';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpeakerServiceService {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getEntities(): Observable<Speaker[]> {
+    return this.http.get<Speaker[]>(Constants.SPEAKERS_API)
+  }
 }
