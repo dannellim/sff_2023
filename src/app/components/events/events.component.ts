71d7329f2e709b427e3274d05966a155685eaf8c
Dannel Lim Khye Hsen <dannel.limkhyehsen@azqore.com>
2023-10-25 22:26:32 +0800

    add filter agenda by date dropdown

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Helper } from 'src/app/helper';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Event } from 'src/app/models/event';
 import { EventServiceService } from 'src/app/services/event/event-service.service';
 import { LoaderService } from 'src/app/services/loader/loader.service';
@@ -11,9 +11,11 @@ import { LoaderService } from 'src/app/services/loader/loader.service';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent {
-  constructor(private router: Router, private eventService: EventServiceService, private loader: LoaderService) { }
-  events : Event[] = [];
-  dates: Date[] = []
+  constructor(private router: Router, private route: ActivatedRoute, private eventService: EventServiceService, private loader: LoaderService) { }
+  events: Event[] = [];
+  dates: Date[] = [];
+  datesDropdown: Date[] = [];
+  dateDropdownText: string = "";
   monthAsString(arg0: number) {
     return Helper.monthAsString(arg0);
   }
@@ -25,10 +27,27 @@ export class EventsComponent {
   }
   ngOnInit(): void {
     this.loader.setLoading(true);
-    this.eventService.getEvents().subscribe(events => {
-      this.events = events;
-      this.getDateArray();
-      this.loader.setLoading(false);
+    this.route.params.subscribe(routeParams => {
+      this.eventService.getEvents().subscribe(events => {
+        this.initDateDropdown(events);
+        const dateJson = routeParams['dateJson'];
+        if (dateJson) {
+          this.events = [];
+          this.dates = [];
+          for (let i = 0; i < events.length; i++) {
+            var [DD, MM, YYYY] = events[i].date.split('/');
+            var date = new Date(YYYY + "-" + MM + "-" + DD);
+            if (date.toJSON() == dateJson) {
+              this.events.push(events[i]);
+            }
+          }
+          var date = new Date(dateJson);
+          this.dateDropdownText = date.getDate()
+            + " " + this.monthAsString(date.getMonth()).substring(0, 3) + " " + date.getFullYear().toString().slice(-2);
+        } else { this.events = events; this.dateDropdownText = "All dates" }
+        this.getDateArray();
+        this.loader.setLoading(false);
+      });
     });
   }
   getDateArray() {
@@ -37,6 +56,15 @@ export class EventsComponent {
       var date = new Date(YYYY + "-" + MM + "-" + DD);
       this.dates.push(date);
     }
+  }
+  initDateDropdown(events: Event[]) {
+    var dates: Date[] = [];
+    for (let i = 0; i < events.length; i++) {
+      var [DD, MM, YYYY] = events[i].date.split('/');
+      var date = new Date(YYYY + "-" + MM + "-" + DD);
+      dates.push(date);
+    }
+    this.datesDropdown = dates.filter((date, i, self) => self.findIndex(d => d.getDate() === date.getDate()) === i);
   }
   register(id: number) {
     this.router.navigate(['/register', id]);
